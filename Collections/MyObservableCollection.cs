57898e99aac04c7712b7beb079c355a08ca875be
Yur4ikOgur4ik <chirtsov.yura@gmail.com>
2025-06-11 10:12:42 +0300

    initial commit

---
+++ Collections/MyObservableCollection.cs
@@ -0,0 +1,15 @@
+﻿using MusicalInstruments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class MyObservableCollection<T> : MyCollection<T>
+        where T: IInit, ICloneable, new()
+    {
+
+    }
+}
